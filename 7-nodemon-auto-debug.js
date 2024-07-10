504c9905ac822b76af0f9f692e91b5cb75a12b1b
mahesh-shakya <mhskhilli2@gmail.com>
2024-07-10 08:54:31 +0530

    nodemon package

---
+++ 7-nodemon-auto-debug.js
@@ -0,0 +1,20 @@
+// install nodemon globally
+// npm install -g nodemon
+// npm install nodemon -g
+// nodemon -v  (check version of nodemon or properly installed or not)
+// If install any package globally, it will install operating system not in our project folder, 
+// we can use anywhere globally installed package, we can use outside the project folder.
+// Globally installed package does not install in project folder.
+
+
+// If you get error while nodemon version checking like this--->
+// nodemon : File C:\Users\MAKS\AppData\Roaming\npm\nodemon.ps1 cannot be loaded because running scripts is disabled on this system. For more information, see about_Execution_Policies at https:/go.microsoft.com/fwlink/?LinkID=135170.
+
+// C:\Users\MAKS\AppData\Roaming\npm\nodemon.ps1 DELETE THIS FILE & check nodemon version 
+
+// nodemon is a tool that helps develop Node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+
+console.log('to see the effect, change in file');
+
+// run file :- nodemon index.js
+// Exit nodemon :- ctrl+c
