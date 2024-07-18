be0431543043fb49e6386c8c9adb6490d741bca1
mahesh-shakya <mhskhilli2@gmail.com>
2024-07-18 08:40:36 +0530

    user api

---
+++ 11-api-main.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+// const url = require('url');
+
+const server = http.createServer((req, res)=>{
+   
+    // Reading file
+    const data = fs.readFileSync("11.1-user-api.json", "utf-8");
+    const objData = JSON.parse(data);   
+    // convert JSON to javascript object        
+       
+
+    if(req.url=="/"){
+        res.end("hello from Homess");
+    }
+    else if(req.url=="/about"){    
+        res.end("hello from About");
+
+    }
+    else if(req.url=="/userapi"){   
+        res.writeHead(200,{"Content-type": "application/json"});
+        res.end(objData[1].employee_name);
+        // res.end("Api Data given below:-\n"+data);
+        // showing api data on web page        
+        // res.end(objData[1].employee_name);
+        // http://127.0.0.1:8080/userapi 
+        // when hit the user api page objData[1].employee_name show       
+        
+    }
+    else{
+        res.writeHead(404, {"Content-type": "text/html"});
+        res.end("<h1> 404 Error Page</h1>");
+    }
+});
+server.listen(8080, "127.0.0.1", ()=>{
+    console.log("port listening");
+    console.log("Go to browser: "+"http://127.0.0.1:8080");
+
+})
+
+
